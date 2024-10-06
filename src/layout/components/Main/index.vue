df08626ffdd0b4d7bd35b481a42899fe6466a7d4
WH934426 <wh934426@outlook.com>
2024-10-06 16:26:40 +0800

    feat: 更新主应用界面并添加页面转场动画

--- src/layout/components/Main/index.vue
+++ src/layout/components/Main/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div>app-main</div>
+	<section class="app-main">
+		<router-view v-slot="{ Component, route }">
+			<transition name="fade-tranform" mode="out-in">
+				<component :is="Component" :key="route.path" />
+			</transition>
+		</router-view>
+	</section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.app-main {
+	height: calc(100% - 64px);
+	overflow-y: auto;
+}
+</style>
