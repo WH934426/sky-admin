5122601a2ea215a2a4a6cbec763870ef356117c1
WH934426 <wh934426@outlook.com>
2024-10-06 15:58:04 +0800

    feat: 集成pinia

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import '@/styles/reset.scss'; // 清除默认样式
 import '@/styles/index.scss'; // 引入全局样式
 import 'element-plus/dist/index.css'; // 组件库样式
 import 'virtual:svg-icons-register'; // 注册svg插件
-import router from './router';
+import router from './router'; // 注册vue-router
+import store from './stores'; // 注册pinia
 
 import { createApp } from 'vue';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
