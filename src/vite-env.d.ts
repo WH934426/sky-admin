d030cae340f09a381048fb3d8d13582aacb6b514
WH934426 <wh934426@outlook.com>
2024-10-06 09:16:11 +0800

    feat: 配置环境变量

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,8 +2,19 @@
 
 // 识别.vue文件
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  const component: DefineComponent<{}, {}, any>
-  export default component
+	import { DefineComponent } from 'vue';
+	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
+}
+
+// 识别.env
+interface ImportMetaEnv {
+	readonly VITE_APP_BASE_URL: string;
+	readonly VITE_APP_API_URL: string;
+	readonly VITE_APP_SOCKET_URL: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
 }
