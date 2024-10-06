303014c57887bdb37f23677c316d40fe63ccf643
WH934426 <wh934426@outlook.com>
2024-10-06 21:29:10 +0800

    fix: 修复app store实例初始化时机错误的问题

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,7 @@
 import { createPinia } from 'pinia';
-import { useAppStore } from './modules/appStore';
+
 const store = createPinia();
 
-const appStore = useAppStore();
-
 export default store;
-export { appStore };
+
+export * from './modules/appStore';
