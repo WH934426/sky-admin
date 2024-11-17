d93a2c0ff5dd441f66412e44bbca4084b35bbdac
WH934426 <wh934426@outlook.com>
2024-11-17 19:01:43 +0800

    feat: 左侧菜单完成

---
+++ src/layout/components/Sidebar/Logo.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { useAppStore } from '@/stores';
+
+// 是否折叠
+const appStore = useAppStore();
+const isCollapse = computed(() => !appStore.isSidebarOpened);
+</script>
+
+<template>
+	<div class="logo">
+		<div v-if="!isCollapse" class="sidebar-logo">
+			<img
+				alt=""
+				src="@/assets/images/white_logo.png"
+				style="width: 131px; height: 40px"
+			/>
+		</div>
+		<div v-else class="sidebar-logo-mini">
+			<img
+				alt=""
+				src="@/assets/images/mini-logo.png"
+				style="width: 30px; height: 30px"
+			/>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+	text-align: center;
+	background-color: #ffc100;
+	padding: 15px 0 0;
+	height: 60px;
+
+	img {
+		display: inline-block;
+	}
+}
+</style>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,7 +1,134 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useAppStore } from '@/stores';
+import type { RouteItem } from '@/types/router.d.ts';
+import Logo from './Logo.vue';
+
+// 是否折叠
+const appStore = useAppStore();
+const isCollapse = computed(() => !appStore.sidebar.opened);
+
+const $route = useRoute();
+const defaultActive = computed(() => $route.path);
+const $router = useRouter();
+
+// 提取后的路由信息接口
+interface RouteVoid {
+	path: string;
+	title: string;
+	icon: string | undefined;
+	order: number;
+}
+
+// 提取所需字段的函数
+function extractRouteInfo(routes: RouteItem[]): RouteVoid[] {
+	return routes.reduce((acc: RouteVoid[], route) => {
+		if (route.children && route.children.length > 0) {
+			// 遍历子路由
+			route.children.forEach((child) => {
+				if (child.meta && child.meta.title && child.meta.order) {
+					// 使用子路由的完整路径
+					const fullPath = `${route.path}/${child.path}`.replace(/\/\//g, '/');
+					acc.push({
+						path: fullPath,
+						title: child.meta.title,
+						icon: child.meta.icon,
+						order: child.meta.order
+					});
+				}
+			});
+		} else if (route.meta && route.meta.title && route.meta.order) {
+			// 如果没有子路由，直接添加当前路由
+			acc.push({
+				path: route.path,
+				title: route.meta.title,
+				icon: route.meta.icon,
+				order: route.meta.order
+			});
+		}
+		return acc;
+	}, []);
+}
+
+const extractedRoutes = computed(() =>
+	extractRouteInfo($router.options.routes as unknown as RouteItem[])
+);
+
+// 确保菜单项按 order 排序
+const sortedExtractedRoutes = computed(() => {
+	return [...extractedRoutes.value].sort((a, b) => a.order - b.order);
+});
+</script>
 
 <template>
-	<div class="sidebar">sidebar</div>
+	<div class="sidebar">
+		<!-- Logo -->
+		<Logo />
+		<!-- 目录 -->
+		<el-scrollbar wrap-class="scrollbar-wrapper">
+			<el-menu
+				:collapse="isCollapse"
+				:collapse-transition="false"
+				:default-active="defaultActive"
+				:router="true"
+			>
+				<el-menu-item
+					v-for="route in sortedExtractedRoutes"
+					:key="route.path"
+					:index="route.path"
+					class="item"
+				>
+					<svg-icon
+						:name="route.icon"
+						class="icon"
+						height="18px"
+						width="18px"
+					/>
+					<span class="title">{{ route.title }}</span>
+				</el-menu-item>
+			</el-menu>
+		</el-scrollbar>
+	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.sidebar {
+	height: 100%;
+	width: 100%;
+}
+
+.el-scrollbar {
+	height: 100%;
+	background-color: rgb(52, 55, 68);
+}
+
+.el-menu {
+	border: none;
+	height: calc(95vh - 23px);
+	width: 100% !important;
+	padding: 47px 15px 0;
+	--el-menu-bg-color: #343744;
+	--el-menu-text-color: #bfcbd9;
+	--el-menu-active-color: #ffc200;
+}
+
+.item {
+	display: flex;
+	align-items: center; // 垂直居中对齐
+	padding: 0 20px; // 内边距可以根据需要调整
+
+	.icon {
+		margin-right: 8px; // 图标和标题之间的间距
+		width: 18px; // 设置图标宽度
+		height: 18px; // 设置图标高度
+		display: inline-flex; // 确保图标是内联元素
+		align-items: center; // 垂直居中对齐
+		justify-content: center; // 水平居中对齐
+	}
+
+	.title {
+		white-space: nowrap; // 防止标题换行
+		overflow: hidden; // 隐藏溢出的内容
+		text-overflow: ellipsis; // 溢出时显示省略号
+	}
+}
+</style>
